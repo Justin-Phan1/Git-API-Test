214ae2f65d0fd564efe0b433844c331c21bfd26f
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2025-09-29 22:39:48 -0400

    new push

--- get_repos_and_commits.py
+++ get_repos_and_commits.py
@@ -11,10 +11,7 @@ def getReposAndCommits(user):
         getcommits_url = "https://api.github.com/repos/" + user + "/" + repo_name + "/commits"
         commits_resp = requests.get(getcommits_url)
         commits = commits_resp.json()
-
-        commit_count = len(commits)
-        result[repo_name] = commit_count
-        
-        print(f"Repo: {repo_name} Number of commits: {commit_count}")
-        
+        num_commit = len(commits)
+        result[repo_name] = num_commit
+        print(f"Repo: {repo_name} Number of commits: {num_commit}")
     return result
